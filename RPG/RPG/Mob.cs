3ef21d4487d505ec580a279fc975dd1d3a19e1ab
DigitalWolrd <paulo.miranda107@gmail.com>
2018-02-07 12:42:39 -0200

    mob ini

---
+++ RPG/RPG/Mob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract public class Mob
+    {
+        private String nome;
+        private int total_HP, total_MP, current_HP, current_MP, Dmg, Def;
+        private int given_XP, lvl;
+    }
+    
+}
