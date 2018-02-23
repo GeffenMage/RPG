165b87abd701cd9396e4780934734cf09c67de95
Daniel Sousa duplat <ddaniel1912@gmail.com>
2018-02-23 16:32:10 -0300

    Pequenas correções para organização de pastas funcionar

--- RPG/RPG/Inimigos/Laposo.cs
+++ RPG/RPG/Inimigos/Laposo.cs
@@ -13,21 +13,21 @@ namespace RPG {
                     Nome = "Laposo Hello";
                     Hp_total = 300; Hp_atual = Hp_total;
                     Mp_total = 150; Mp_atual = Mp_total;
-                    Given_xp = 100; Lvl1 = 5;
+                    Given_xp = 100; Lvl = 5;
                     Base_def = 20; Base_dmg = 50;
                     break;
                 case 2:
                     Nome = "Laposo World";
                     Hp_total = 450; Hp_atual = Hp_total;
                     Mp_total = 200; Mp_atual = Mp_total;
-                    Given_xp = 300; Lvl1 = 10;
+                    Given_xp = 300; Lvl = 10;
                     Base_def = 30; Base_dmg = 70;
                     break;
                 case 3:
                     Nome = "Laposo HelloWorld";
                     Hp_total = 600; Hp_atual = Hp_total;
                     Mp_total = 300; Mp_atual = Mp_total;
-                    Given_xp = 700; Lvl1 = 15;
+                    Given_xp = 700; Lvl = 15;
                     Base_def = 40; Base_dmg = 90;
                     break;
             }
--- RPG/RPG/Inimigos/Ogro.cs
+++ RPG/RPG/Inimigos/Ogro.cs
@@ -13,21 +13,21 @@ namespace RPG {
                     Nome = "Ogro Verde";
                     Hp_total = 800; Hp_atual = Hp_total;
                     Mp_total = 100; Mp_atual = Mp_total;
-                    Given_xp = 1000; Lvl1 = 20;
+                    Given_xp = 1000; Lvl = 20;
                     Base_def = 60; Base_dmg = 110;
                     break;
                 case 2:
                     Nome = "Ogro Negro";
                     Hp_total = 1000; Hp_atual = Hp_total;
                     Mp_total = 150; Mp_atual = Mp_total;
-                    Given_xp = 1300; Lvl1 = 25;
+                    Given_xp = 1300; Lvl = 25;
                     Base_def = 70; Base_dmg = 130;
                     break;
                 case 3:
                     Nome = "Ogro Chefe";
                     Hp_total = 1200; Hp_atual = Hp_total;
                     Mp_total = 150; Mp_atual = Mp_total;
-                    Given_xp = 1700; Lvl1 = 30;
+                    Given_xp = 1700; Lvl = 30;
                     Base_def = 80; Base_dmg = 150;
                     break;
             }
