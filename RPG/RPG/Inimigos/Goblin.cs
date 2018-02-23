fe41186c7e72d90b868a3b51b4f828f39ab303de
DigitalWord <paulo.miranda107@gmail.com>
2018-02-23 16:22:42 -0300

    Tentativa de correção

---
+++ RPG/RPG/Inimigos/Goblin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Personagens.Inimigos {
+    class Goblin : Mob {
+
+        public Goblin(int lvl) {
+            switch (lvl){
+                case 1:
+                    Nome = "Goblin";
+                    Hp_total = 300; Hp_atual = Hp_total;
+                    Mp_total = 150; Mp_atual = Mp_total;
+                    Given_xp = 100; Lvl1 = 5;
+                    Base_def = 20; Base_dmg = 50;
+                    break;
+                case 2:
+                    Nome = "Goblin";
+                    Hp_total = 450; Hp_atual = Hp_total;
+                    Mp_total = 200; Mp_atual = Mp_total;
+                    Given_xp = 300; Lvl1 = 10;
+                    Base_def = 30; Base_dmg = 70;
+                    break;
+                case 3:
+                    Nome = "Goblin";
+                    Hp_total = 600; Hp_atual = Hp_total;
+                    Mp_total = 300; Mp_atual = Mp_total;
+                    Given_xp = 700; Lvl1 = 15;
+                    Base_def = 40; Base_dmg = 90;
+                    break;
+            }
+            
+        }
+
+    }
+}
